6779e7fa531b9968bf88be21baf1981a3fa81947
Yash Sharma <WeebGenix@proton.me>
2024-10-29 12:01:14 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
 
-# Install Python 3.11.3
-
-sudo apt update
-
 # -- Important Build essential files -- # 
 
+sudo apt update
 sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
+sudo apt-get install -y software-properties-common
 
-wget https://www.python.org/ftp/python/3.11.3/Python-3.11.3.tgz
-tar -xf Python-3.11.3.tgz
-cd Python-3.11.3
-./configure --enable-optimizations
-make -j 12
-sudo make altinstall
-cd ..
-sudo rm -r Python-3.11.3
+# Install Python 3.11.3
 
+sudo add-apt-repository -y ppa:deadsnakes/ppa
+sudo apt-get install -y python3.11
 
 # Installing ffmpeg 
+
 sudo apt install -y ffmpeg
 
 # Installing LibTorrent 
+
 sudo apt install -y python3-libtorrent
 
 # Installing Required Pypi Packages
